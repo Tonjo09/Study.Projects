f06c6c312702783b54558b22d395f8420815c05d
Tony <ttonyng@gmail.com>
2024-03-11 23:17:19 +0100

    fullfører koden

--- CsharpProjects/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ CsharpProjects/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca48377f48dc2dc9c873e8abd9f49cbd7cfce5ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24988700825bfa0c0c18178e18af3eb30f761ff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
