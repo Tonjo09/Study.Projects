b5995057d5a1688b03d749c88fb96ad6e59a3545
Tony Tuong Nguyen <s341492@oslomet.no>
2024-03-14 20:20:14 +0100

    Lagt til metode som regner ut student sin totale grad

--- CsharpProjects/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/Program.cs
+++ CsharpProjects/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/Program.cs
@@ -18,6 +18,8 @@ This C# console application is designed to:
     Emma:           85.6    B
     Logan:          91.2    A-
 */
+using System.Runtime.InteropServices.Marshalling;
+
 int examAssignments = 5;
 
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
@@ -59,28 +61,44 @@ foreach (string name in studentNames)
     else if (currentStudent == "Logan")
         studentScores = loganScores;
 
-    int sumAssignmentScores = 0;
-
-    decimal currentStudentGrade = 0;
 
     int gradedAssignments = 0;
+    int gradedExtraCreditAssignments = 0;
+
+
+    int sumExamScores = 0;
+    int sumExtraCreditScores = 0;
+
+    decimal currentStudentGrade = 0;
+    decimal currentStudentExamScore = 0;
+    decimal currentStudentExtraCreditScore = 0;
+
+
 
     /* 
     the inner foreach loop sums assignment scores
     extra credit assignments are worth 10% of an exam score
+
+    - Need to implement for just extra credit
     */
     foreach (int score in studentScores)
     {
         gradedAssignments += 1;
 
         if (gradedAssignments <= examAssignments)
-            sumAssignmentScores += score;
+        {
+            sumExamScores += score;
+        }
 
         else
-            sumAssignmentScores += score / 10;
+        {
+            gradedExtraCreditAssignments += 1;
+            sumExtraCreditScores += score;
+        }
+
     }
 
-    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+    currentStudentGrade = ((decimal)sumExamScores + ((decimal)sumExtraCreditScores / 10)) / examAssignments;
 
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
@@ -124,7 +142,7 @@ foreach (string name in studentNames)
     // Student         Grade
     // Sophia:         92.2    A-
 
-    Console.WriteLine($"{currentStudent}\t\t\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentExamScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{currentStudentExtraCreditScore}");
 }
 
 // required for running in VS Code (keeps the Output windows open to view results)
