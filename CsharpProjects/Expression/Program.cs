3a01dae620cec0fbbf14923176b86c291a4ac493
Tony Tuong Nguyen <s341492@oslomet.no>
2024-03-16 21:29:53 +0100

    Modul for expressions

--- CsharpProjects/Expression/Program.cs
+++ CsharpProjects/Expression/Program.cs
@@ -1,4 +1,3 @@
-﻿string value1 = " a";
-string value2 = "A ";
-Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
-
+﻿string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox"));
