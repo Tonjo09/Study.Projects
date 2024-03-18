961c7cb0aec4e4776871f6311cd6afefadb8fa1c
Tony <ttonyng@gmail.com>
2024-03-18 08:34:19 +0100

    Fullført code challenge

--- CsharpProjects/Expression/Program.cs
+++ CsharpProjects/Expression/Program.cs
@@ -1,3 +1,6 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(pangram.Contains("fox") == false);
-Console.WriteLine(!pangram.Contains("fox"));
+﻿Random coin = new Random();
+int flip = coin.Next(0, 2);
+Console.WriteLine((flip == 0) ? "heads" : "tails");
+
+Random coin1 = new Random();
+Console.WriteLine((coin1.Next(0, 2) == 0) ? "heads" : "tails");
