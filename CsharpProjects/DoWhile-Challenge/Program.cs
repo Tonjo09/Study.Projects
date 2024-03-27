70f45b1698109d3287d2cd898a5d2521407e89d2
Tony Tuong Nguyen <s341492@oslomet.no>
2024-03-27 20:20:24 +0100

    Fullført challenge
    https://learn.microsoft.com/nb-no/training/modules/csharp-do-while/3-challenge

---
+++ CsharpProjects/DoWhile-Challenge/Program.cs
@@ -0,0 +1,26 @@
+﻿int heroHP = 10;
+int monsterHP = 10;
+
+Random attack = new Random();
+
+
+do
+{
+    int heroAttack = attack.Next(1, 11);
+    monsterHP -= heroAttack;
+    Console.WriteLine($"Monster was damaged and lost {heroAttack} health and now has {monsterHP} health.");
+
+
+    if (monsterHP <= 0) continue;
+
+    int monsterAttack = attack.Next(1, 11);
+    heroHP -= monsterAttack;
+    Console.WriteLine($"Hero was damaged and lost {monsterAttack} health and now has {heroHP} health.");
+
+} while (heroHP > 0 && monsterHP > 0);
+
+Console.WriteLine(heroHP > monsterHP ? "Hero wins!" : "Monster wins!");
+
+
+
+
