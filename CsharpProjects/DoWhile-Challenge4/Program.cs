bbaa75c513d4f9a7c1cf59bf2abe30dffa8d1113
Tony Tuong Nguyen <s341492@oslomet.no>
2024-04-03 23:15:59 +0200

    .

---
+++ CsharpProjects/DoWhile-Challenge4/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int periodLocation = 0;
+
+foreach (string myString in myStrings)
+{
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+
+    // extract sentences from each string and display each one at a time
+    while (periodLocation != -1)
+    {
+
+        // first sentence is left for the first period location
+        mySentence = myString.Remove(periodLocation);
+
+        // takes remainder to right after period
+        myString = myString.Substring(periodLocation + 1);
+
+
+
+
+        Console.WriteLine(mySentence);
+    }
+
+
+}
