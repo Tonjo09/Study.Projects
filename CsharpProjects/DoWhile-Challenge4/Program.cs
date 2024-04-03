ddd76e67ee8b5f706853cddd6755832998c3b176
Tony Tuong Nguyen <s341492@oslomet.no>
2024-04-03 23:41:13 +0200

    Done

--- CsharpProjects/DoWhile-Challenge4/Program.cs
+++ CsharpProjects/DoWhile-Challenge4/Program.cs
@@ -1,8 +1,10 @@
 ﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
 int periodLocation = 0;
+string myString = "";
 
-foreach (string myString in myStrings)
+for (int i = 0; i < myStrings.Length; i++)
 {
+    myString = myStrings[i];
     periodLocation = myString.IndexOf(".");
 
     string mySentence;
@@ -17,11 +19,15 @@ foreach (string myString in myStrings)
         // takes remainder to right after period
         myString = myString.Substring(periodLocation + 1);
 
+        // remove leading white-space from myString
+        myString = myString.TrimStart();
 
-
+        // update period
+        periodLocation = myString.IndexOf(".");
 
         Console.WriteLine(mySentence);
     }
 
-
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
 }
