0055c0648da3f27a9c80ba082525a99556f08e40
Tony Tuong Nguyen <s341492@oslomet.no>
2024-04-06 23:48:38 +0200

    .

--- CsharpProjects/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/Program.cs
+++ CsharpProjects/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/Program.cs
@@ -172,6 +172,20 @@ do
             do
             {
                 int petAge;
+                Console.WriteLine("Enter the pet's age or enter ? if unknown");
+                readResult = Console.ReadLine();
+                if (readResult != null)
+                {
+                    animalAge = readResult;
+                    if (animalAge != "?")
+                    {
+                        validEntry = int.TryParse(animalAge, out petAge);
+                    }
+                    else
+                    {
+                        validEntry = true;
+                    }
+                }
             } while (validEntry == false);
 
             while (anotherPet == "y" && petCount < maxPets)
