05dcd70c6929de394ea6db297dc7f7e904ee4817
Tony Tuong Nguyen <s341492@oslomet.no>
2024-04-13 01:02:33 +0200

    https://learn.microsoft.com/nb-no/training/modules/csharp-convert-cast/4-challenge

--- CsharpProjects/Test/Program.cs
+++ CsharpProjects/Test/Program.cs
@@ -1,4 +1,5 @@
-﻿string value = "bad";
+﻿/*
+string value = "bad";
 int result = 1;
 if (int.TryParse(value, out result))
 {
@@ -11,3 +12,25 @@ else
 
 if (result > 0)
     Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+     */
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number;
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"{message}");
+Console.WriteLine($"{total}");
